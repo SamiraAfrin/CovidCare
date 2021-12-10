1ba54c9e1a66f73fcc4789cd5f33e7ebc0d7d109
SamiraAfrin <samira.afrin.alam.shopnil@g.bracu.ac.bd>
2021-12-10 11:23:23 +0600

    Dashboard URL

---
+++ CovidCare/Controllers/AdminActivitiesController.cs
@@ -0,0 +1,27 @@
+﻿using CovidCare.Data;
+using CovidCare.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidCare.Controllers
+{
+    public class AdminActivitiesController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public AdminActivitiesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationUser> objList = _db.ApplicationUser;
+            return View(objList);
+        }
+    }
+}
